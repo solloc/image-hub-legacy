d049c3553c372f414e39c367ec8fa41d1300f623
solloc <matthias@maisch.me>
2017-03-26 20:37:11 +0200

    Started with file upload + Exchanged style with SASS for CSS

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
+
 export class FileUploadComponent implements OnInit {
+
+  filesToUpload: File[] = File[''];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  static onSubmit(): void {
+    console.log('submitted');
+    // for (let fileToUpload of this.filesToUpload) {
+    // }
+  }
+
+  onChange(event: any):void {
+    console.log('files changed');
+    this.filesToUpload = event.target.files;
+    // console.log();
+    for (let fileToUpload of this.filesToUpload) {
+      console.log('uploaded: ' + fileToUpload.name);
+    }
   }
 
 }
